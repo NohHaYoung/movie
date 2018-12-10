90f9701caf33eb4eed5037fe8e3c0427eb8394c1
NohHaYoung <42965065+NohHaYoung@users.noreply.github.com>
2018-12-10 22:17:59 +0900

    while문 해결

--- movie.c
+++ movie.c
@@ -18,16 +18,16 @@ void* mv_genMvInfo(char* name, float score, int runTime, char* country)
 	//allocate memory and set the member variables
 	mvPtr = (movInfo_t*)malloc(sizeof(movInfo_t));
 	
-	if(mvPtr = NULL){
+	if(mvPtr == NULL){
 		printf("ERROR : No memory allocated.");
 	}
+	
 	
 	strcpy(mvPtr->madeIn, country);
 	strcpy(mvPtr->name, name);
 	mvPtr->runTime = runTime;
 	mvPtr->score = score;
 	
-		
 	return (void*)mvPtr;
 
 }
